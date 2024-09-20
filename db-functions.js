b9b4b52b941b337fa3443f8614f4500b9d1667ab
gavand-sagar <gavand.sagar.s@gmail.com>
2024-09-20 20:26:22 +0530

    database changed

--- db-functions.js
+++ db-functions.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 export  async function getDataBase(){
-    let client = new MongoClient("mongodb://127.0.0.1:27017")
+    let client = new MongoClient("mongodb+srv://admin:123@cluster0.dnyhi.mongodb.net/")
     let connection = await client.connect();
     let db = connection.db("lucifer");
     return db;
